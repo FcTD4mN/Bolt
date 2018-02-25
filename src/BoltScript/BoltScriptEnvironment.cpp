68959874dcaeafd0d8abb21baf90d674e836ae57
LaylDupont <layldupont@gmail.com>
2018-02-25 22:06:50 +0100

    simple fun parsing, try typing BOLT

--- src/BoltScript/BoltScriptEnvironment.cpp
+++ src/BoltScript/BoltScriptEnvironment.cpp
@@ -30,6 +30,15 @@ void
 cBoltScriptEnvironment::ProcessRawString( const  std::string&  iStr )
 {
     mOutputRedirectionFunction( iStr );
+
+    if( iStr == "BOLT" )
+    {
+        mOutputRedirectionFunction( "xxx  xxxx x    xxxxx" );
+        mOutputRedirectionFunction( "x x  x  x x      x  " );
+        mOutputRedirectionFunction( "xxxx x  x x      x  " );
+        mOutputRedirectionFunction( "x  x x  x x      x  " );
+        mOutputRedirectionFunction( "xxxx xxxx xxxx   x  " );
+    }
 }
 
 
