ec32128b07565daade475794f9615fd24e1fdc1c
D4mN <fctwarrior@gmail.com>
2018-02-26 22:33:38 +0100

    Added entity vector edition

--- src/ECS/Entity.cpp
+++ src/ECS/Entity.cpp
@@ -15,6 +15,247 @@ cEntity::~cEntity()
 {
     for( auto it = mComponents.begin(); it != mComponents.end(); ++it )
     {
+        delete  it->value;
+    }
+}
+
+
+cEntity::cEntity( cWorld* iWorld ) :
+    mWorld( iWorld ),
+    mID( "idontknow" + std::to_string( sgEntityCount ) ),
+    mComponents(),
+    mTags(),
+    mObserverSystems(),
+    mLoaded( false ),
+    mDead( false )
+{
+    ++sgEntityCount;
+}
+
+
+cEntity::cEntity( const cEntity& iEntity ) :
+    mWorld( iEntity.mWorld ),
+    mID( iEntity.mID + "c" + std::to_string( sgEntityCount ) ),
+    mComponents(),                                  // This will need to get copied
+    mTags( iEntity.mTags ),                         // This can be copied, as there is no pointer
+    mObserverSystems( iEntity.mObserverSystems ),   // Same systems will observe this entity
+    mLoaded( iEntity.mLoaded ),
+    mDead( iEntity.mDead )
+{
+    for( auto it = iEntity.mComponents.begin(); it != iEntity.mComponents.end(); ++it )
+    {
+        sPair pair;
+        pair.key = it->key;
+        pair.value = it->value->Clone();
+        mComponents.push_back( pair );
+    }
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------- Copy
+// -------------------------------------------------------------------------------------
+
+
+cEntity*
+cEntity::Clone()
+{
+    return  new cEntity( *this );
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------- Components
+// -------------------------------------------------------------------------------------
+
+
+void
+cEntity::AddComponent( cComponent * iComponent )
+{
+    sPair pair;
+    pair.key = iComponent->Name();
+    pair.value = iComponent;
+    mComponents.push_back( pair );
+}
+
+
+void
+cEntity::RemoveComponent( cComponent * iComponent )
+{
+    for( int i = 0; i < mComponents.size(); ++i )
+    {
+        if( mComponents[ i ].key == iComponent->Name() && mComponents[ i ].value == iComponent )
+            mComponents.erase( mComponents.begin() + i );
+    }
+}
+
+
+void
+cEntity::RemoveComponentByName( const std::string & iComponentName )
+{
+    for( int i = 0; i < mComponents.size(); ++i )
+    {
+        if( mComponents[ i ].key == iComponentName )
+            mComponents.erase( mComponents.begin() + i );
+    }
+}
+
+
+cComponent*
+cEntity::GetComponentByName( const std::string & iComponentName )
+{
+    for( int i = 0; i < mComponents.size(); ++i )
+    {
+        if( mComponents[ i ].key == iComponentName )
+            return  mComponents[ i ].value;
+    }
+
+    return  0;
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------- Tags
+// -------------------------------------------------------------------------------------
+
+
+void
+cEntity::AddTag( const std::string & iTag )
+{
+    mTags.push_back( iTag );
+}
+
+
+void
+cEntity::RemoveTag( const std::string & iTag )
+{
+    for( int i = 0; i < mTags.size(); ++i )
+    {
+        if( mTags[ i ] == iTag )
+            mTags.erase( mTags.begin() + i );
+    }
+}
+
+
+bool
+cEntity::HasTag( const std::string & iTag )
+{
+    for( int i = 0; i < mTags.size(); ++i )
+    {
+        if( mTags[ i ] == iTag )
+            return  true;
+    }
+
+    return  false;
+}
+
+
+// -------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------ Access
+// -------------------------------------------------------------------------------------
+
+
+bool
+cEntity::IsDead() const
+{
+    return mDead;
+}
+
+
+void
+cEntity::SetLoaded()
+{
+    mLoaded = true;
+}
+
+
+const std::string&
+cEntity::ID() const
+{
+    return  mID;
+}
+
+
+// -------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------ Input/Output
+// -------------------------------------------------------------------------------------
+
+
+void
+cEntity::SaveXML( tinyxml2::XMLElement * iNode, tinyxml2::XMLDocument* iDocument )
+{
+    iNode->SetAttribute( "id", mID.c_str() );
+
+    tinyxml2::XMLElement* components = iDocument->NewElement( "components" );
+
+    for( auto it = mComponents.begin(); it != mComponents.end(); ++it )
+    {
+        // Thing is wherever you do a mComponents[ "something" ], it creates a key value pair : {"something", NULL}
+        if( it->value )
+        {
+            tinyxml2::XMLElement* component = iDocument->NewElement( "component" );
+            it->value->SaveXML( component, iDocument );
+            components->LinkEndChild( component );
+        }
+    }
+
+    iNode->LinkEndChild( components );
+
+    tinyxml2::XMLElement* tags = iDocument->NewElement( "tags" );
+
+    for( auto it = mTags.begin(); it != mTags.end(); ++it )
+    {
+        tinyxml2::XMLElement* tag = iDocument->NewElement( "tag" );
+        tag->SetAttribute( "name", it->c_str() );
+        tags->LinkEndChild( tag );
+    }
+
+    iNode->LinkEndChild( tags );
+}
+
+
+void
+cEntity::LoadXML( tinyxml2::XMLElement * iNode )
+{
+    mID = iNode->Attribute( "id" );
+    tinyxml2::XMLElement* components = iNode->FirstChildElement( "components" );
+
+    for( tinyxml2::XMLElement* component = components->FirstChildElement( "component" ); component; component = component->NextSiblingElement() )
+    {
+        cComponent* comp = cComponentRegistry::Instance()->CreateComponentFromName( component->Attribute( "name" ) );
+        if( comp )
+        {
+            comp->LoadXML( component );
+            AddComponent( comp );
+        }
+    }
+
+    tinyxml2::XMLElement* tags = iNode->FirstChildElement( "tags" );
+
+    for( tinyxml2::XMLElement* tag = tags->FirstChildElement( "tag" ); tag; tag = tag->NextSiblingElement() )
+    {
+        AddTag( tag->Attribute( "name" ) );
+    }
+}
+
+
+
+
+// =================================================
+// ========== VERSION UNORDERED_MAP ================
+// =================================================
+
+
+
+/*
+// -------------------------------------------------------------------------------------
+// ------------------------------------------------------------ Construction/Destruction
+// -------------------------------------------------------------------------------------
+
+cEntity::~cEntity()
+{
+    for( auto it = mComponents.begin(); it != mComponents.end(); ++it )
+    {
         delete  it->second;
     }
 }
@@ -213,3 +454,5 @@ cEntity::LoadXML( tinyxml2::XMLElement * iNode )
         AddTag( tag->Attribute( "name" ) );
     }
 }
+
+*/
--- src/ECS/Entity.h
+++ src/ECS/Entity.h
@@ -9,6 +9,16 @@ class cComponent;
 class cSystem;
 class cWorld;
 
+namespace
+{
+    struct sPair
+    {
+        std::string key;
+        cComponent* value;
+    };
+}
+
+
 class cEntity
 {
 
@@ -47,14 +57,71 @@ public:
     virtual  void LoadXML( tinyxml2::XMLElement* iNode );
 
 private:
-    cWorld *                                        mWorld;             // To call for updates if entity changes
+    cWorld *                    mWorld;             // To call for updates if entity changes
 
-    std::string                                     mID;
-    std::unordered_map< std::string, cComponent* >  mComponents;
-    std::unordered_map< std::string, bool >         mTags;              // Tags are "boolean" components, to avoid creating objects with no data.Gives a property such as lootable, or killable
-    std::vector< cSystem* >                         mObserverSystems;   // Entity knows which systems are observing, when destroyed, an entity can remove itself from system directly->fast
-    bool                                            mLoaded;            // Allows creating an entity (ak, adding tags and components) without refreshing systems every time
-    bool                                            mDead;              // The entity is dead and shall be removed from world
+    std::string                 mID;
+    std::vector< sPair >        mComponents;
+    std::vector< std::string >  mTags;              // Tags are "boolean" components, to avoid creating objects with no data.Gives a property such as lootable, or killable
+    std::vector< cSystem* >     mObserverSystems;   // Entity knows which systems are observing, when destroyed, an entity can remove itself from system directly->fast
+    bool                        mLoaded;            // Allows creating an entity (ak, adding tags and components) without refreshing systems every time
+    bool                        mDead;              // The entity is dead and shall be removed from world
 
 };
+
+
+// =================================================
+// ========== VERSION UNORDERED_MAP ================
+// =================================================
+
+/*
+    class cEntity
+    {
+
+    public:
+        // Contruction/Destruction
+        ~cEntity();
+        cEntity( cWorld* iWorld );
+        cEntity( const cEntity& iEntity );
+
+    public:
+        // Copy
+        cEntity * Clone();
+
+    public:
+        // Components
+        void  AddComponent( cComponent* iComponent );
+        void  RemoveComponent( cComponent* iComponent );
+        void  RemoveComponentByName( const std::string& iComponentName );
+        cComponent*  GetComponentByName( const std::string& iComponentName );
+
+    public:
+        // Tags
+        void  AddTag( const std::string& iTag );
+        void  RemoveTag( const std::string& iTag );
+        bool  HasTag( const std::string& iTag );
+
+    public:
+        // Access/Get
+        bool IsDead() const;
+        void SetLoaded();
+        const std::string& ID() const;
+
+    public:
+        // Input/Output
+        virtual  void SaveXML( tinyxml2::XMLElement* iNode, tinyxml2::XMLDocument* iDocument );
+        virtual  void LoadXML( tinyxml2::XMLElement* iNode );
+
+    private:
+        cWorld * mWorld;             // To call for updates if entity changes
+
+        std::string                                     mID;
+        std::unordered_map< std::string, cComponent* >  mComponents;
+        std::unordered_map< std::string, bool >         mTags;              // Tags are "boolean" components, to avoid creating objects with no data.Gives a property such as lootable, or killable
+        std::vector< cSystem* >                         mObserverSystems;   // Entity knows which systems are observing, when destroyed, an entity can remove itself from system directly->fast
+        bool                                            mLoaded;            // Allows creating an entity (ak, adding tags and components) without refreshing systems every time
+        bool                                            mDead;              // The entity is dead and shall be removed from world
+
+    };
+*/
+
 
