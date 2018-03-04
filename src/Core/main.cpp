c77152c19ec77f76f2d0878233819336fd64da38
LaylDupont <layldupont@gmail.com>
2018-03-04 20:36:11 +0100

    clean python tests

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -15,36 +15,6 @@
 
 #include "Benchmark/Benchmark.h"
 
-extern "C" {
-static int numargs=0;
-
-/* Return the number of arguments of the application command line */
-static PyObject*
-emb_numargs(PyObject *self, PyObject *args)
-{
-    if(!PyArg_ParseTuple(args, ":numargs"))
-        return NULL;
-    return PyLong_FromLong(numargs);
-}
-
-static PyMethodDef EmbMethods[] = {
-    {"numargs", emb_numargs, METH_VARARGS,
-     "Return the number of arguments received by the process."},
-    {NULL, NULL, 0, NULL}
-};
-
-static PyModuleDef EmbModule = {
-    PyModuleDef_HEAD_INIT, "emb", NULL, -1, EmbMethods,
-    NULL, NULL, NULL, NULL
-};
-
-static PyObject*
-PyInit_emb(void)
-{
-    return PyModule_Create(&EmbModule);
-}
-
-}
 int
 main(int argc, char *argv[])
 {
@@ -72,10 +42,6 @@ main(int argc, char *argv[])
     }
     Py_SetProgramName(program);
     Py_Initialize();
-
-    numargs = argc;
-    PyImport_AppendInittab("emb", &PyInit_emb);
-
     PyRun_SimpleString("import ctypes\n");
 
     ////////////////////////////////////////////////////////////
