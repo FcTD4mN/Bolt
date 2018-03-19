569666b78abeb227c2a3e21d1f5f5eb19dc8e99f
LaylDupont <cberthaud@tvpaint.com>
2018-03-19 23:23:37 +0100

    wip: some VBO refactoring

--- src/Core/GameMockup/OpenGLRenderSceneScreen.cpp
+++ src/Core/GameMockup/OpenGLRenderSceneScreen.cpp
@@ -76,6 +76,20 @@ cOpenGLRenderSceneScreen::Initialize()
     double ratio = double(window->getSize().x) / double(window->getSize().y);
     gluPerspective(50.f, ratio, 5.f, 500.f);
     glewInit();
+
+    sf::Vector3f Vertices[6];
+    Vertices[0] = sf::Vector3f(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = sf::Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[2] = sf::Vector3f(0.0f, 1.0f, 0.0f);
+    Vertices[3] = sf::Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[4] = sf::Vector3f(0.0f, 1.0f, 0.0f);
+    Vertices[5] = sf::Vector3f(5.0f, 0.0f, 0.0f);
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 }
 
 
@@ -113,20 +127,7 @@ cOpenGLRenderSceneScreen::Draw( sf::RenderTarget* iRenderTarget )
     mMap.DirectDraw();
     glPopMatrix();
 
-    sf::Vector3f Vertices[6];
-    Vertices[0] = sf::Vector3f(-1.0f, -1.0f, 0.0f);
-    Vertices[1] = sf::Vector3f(1.0f, -1.0f, 0.0f);
-    Vertices[2] = sf::Vector3f(0.0f, 1.0f, 0.0f);
-    Vertices[3] = sf::Vector3f(1.0f, -1.0f, 0.0f);
-    Vertices[4] = sf::Vector3f(0.0f, 1.0f, 0.0f);
-    Vertices[5] = sf::Vector3f(5.0f, 0.0f, 0.0f);
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glDisableVertexAttribArray(0);
 
