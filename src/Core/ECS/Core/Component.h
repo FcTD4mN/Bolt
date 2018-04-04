213c1ff2f5038311e0e181e0fbe351fa06500246
D4mN <fctwarrior@gmail.com>
2018-04-04 17:18:26 +0200

    Update Component.h

--- src/Core/ECS/Core/Component.h
+++ src/Core/ECS/Core/Component.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Base/PrimitiveType.h"
+
 #include <string>
 #include "tinyxml2.h"
 
@@ -29,3 +31,31 @@ protected:
     std::string mName;
 };
 
+
+class cComponentGeneric
+{
+public:
+    // Contruction/Destruction
+    virtual  ~cComponent() = 0;
+    cComponent( const std::string& iName );
+    cComponent( const cComponent& iComponent );
+
+public:
+    // Copy
+    virtual  cComponent* Clone() = 0;
+
+public:
+    // Access/Get
+    const  std::string&  Name() const;
+    const cVariant&     GetVar( const std::string& iVarName ) const;
+    void                SetVar( const std::string& iVarName, const cVariant& iValue );
+
+public:
+    // Input/Output
+    virtual  void SaveXML( tinyxml2::XMLElement* iNode, tinyxml2::XMLDocument* iDocument );
+    virtual  void LoadXML( tinyxml2::XMLElement* iNode );
+
+protected:
+    std::string mName;
+    std::unordered_map< std::string, cVariant > mVars;
+};
