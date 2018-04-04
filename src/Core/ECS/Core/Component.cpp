281a1e0a772bf199df6996be4ef663114b6e159a
D4mN <fctwarrior@gmail.com>
2018-04-04 17:18:12 +0200

    CommiT2

--- src/Core/ECS/Core/Component.cpp
+++ src/Core/ECS/Core/Component.cpp
@@ -1,61 +1,139 @@
-#pragma once
+#include "Component.h"
 
-#include "Base/PrimitiveType.h"
+// -------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------ Construction
+// -------------------------------------------------------------------------------------
 
-#include <string>
-#include "tinyxml2.h"
 
-class cComponent
+cComponent::~cComponent()
 {
-
-public:
-    // Contruction/Destruction
-    virtual  ~cComponent() = 0;
-    cComponent( const std::string& iName );
-    cComponent( const cComponent& iComponent );
-
-public:
-    // Copy
-    virtual  cComponent* Clone() = 0;
-
-public:
-    // Access/Get
-    const  std::string&  Name() const;
-
-public:
-    // Input/Output
-    virtual  void SaveXML( tinyxml2::XMLElement* iNode, tinyxml2::XMLDocument* iDocument );
-    virtual  void LoadXML( tinyxml2::XMLElement* iNode );
-
-protected:
-    std::string mName;
-};
+}
 
 
-class cComponentGeneric
+cComponent::cComponent( const std::string& iName ) :
+    mName( iName )
 {
-public:
-    // Contruction/Destruction
-    virtual  ~cComponent() = 0;
-    cComponent( const std::string& iName );
-    cComponent( const cComponent& iComponent );
+}
 
-public:
-    // Copy
-    virtual  cComponent* Clone() = 0;
 
-public:
-    // Access/Get
-    const  std::string&  Name() const;
-    const cVariant&     GetVar( const std::string& iVarName ) const;
-    void                SetVar( const std::string& iVarName, const cVariant& iValue );
+cComponent::cComponent( const cComponent & iComponent ) :
+    mName( iComponent.mName )
+{
+}
 
-public:
-    // Input/Output
-    virtual  void SaveXML( tinyxml2::XMLElement* iNode, tinyxml2::XMLDocument* iDocument );
-    virtual  void LoadXML( tinyxml2::XMLElement* iNode );
 
-protected:
-    std::string mName;
-    std::unordered_map< std::string, cVariant > mVars;
-};
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------- Access/Get
+// -------------------------------------------------------------------------------------
+
+
+const std::string&
+cComponent::Name() const
+{
+    return  mName;
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------- Access/Get
+// -------------------------------------------------------------------------------------
+
+
+void
+cComponent::SaveXML( tinyxml2::XMLElement * iNode, tinyxml2::XMLDocument* iDocument )
+{
+    iNode->SetAttribute( "name", mName.c_str() );
+}
+
+
+void
+cComponent::LoadXML( tinyxml2::XMLElement * iNode )
+{
+    mName = iNode->Attribute( "name" );
+}
+
+
+// -------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------ Construction
+// -------------------------------------------------------------------------------------
+
+
+cComponentGeneric::~cComponentGeneric()
+{
+}
+
+
+cComponentGeneric::cComponentGeneric( const std::string& iName ) :
+    mName( iName )
+{
+}
+
+
+cComponentGeneric::cComponentGeneric( const cComponentGeneric & iComponent ) :
+    mName( iComponent.mName )
+{
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------- Access/Get
+// -------------------------------------------------------------------------------------
+
+
+const std::string&
+cComponentGeneric::Name() const
+{
+    return  mName;
+}
+
+
+const cVariant&
+cComponentGeneric::GetVar( const std::string& iVarName ) const
+{
+    return  mVars[ iVarName ];
+}
+
+
+void
+cComponentGeneric::SetVar( const std::string& iVarName, const cVariant& iValue )
+{
+    mVars[ iVarName ] = iValue;
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------- Access/Get
+// -------------------------------------------------------------------------------------
+
+
+void
+cComponentGeneric::SaveXML( tinyxml2::XMLElement * iNode, tinyxml2::XMLDocument* iDocument )
+{
+    iNode->SetAttribute( "name", mName.c_str() );
+
+    tinyxml2::XMLElement* vars = iDocument->NewElement( "variables" );
+    for( auto& var : mVars )
+    {
+        tinyxml2::XMLElement* variable = iDocument->NewElement( "variable" );
+
+            var->SaveXML( variable, iDocument );
+
+        vars->LinkEndChild( variable );
+    }
+
+    iNode->LinkEndChild( vars );
+}
+
+
+void
+cComponentGeneric::LoadXML( tinyxml2::XMLElement * iNode )
+{
+    mName = iNode->Attribute( "name" );
+
+    tinyxml2::XMLElement* variables = iNode->FirstChildElement( "variables" );
+    for( tinyxml2::XMLElement* variable = variables->FirstChildElement( "variable" ); variable; variable = variable->NextSiblingElement() )
+    {
+        cVariant var();
+        var.LoadXML( variable );
+    }
+}
