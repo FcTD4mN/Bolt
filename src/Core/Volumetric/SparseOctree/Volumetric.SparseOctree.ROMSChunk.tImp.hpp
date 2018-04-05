34527fef1ac04bf5302989ea562bf8b94db0bfd4
LaylDupont <cberthaud@tvpaint.com>
2018-04-05 22:43:00 +0200

    feat:wip: basis for chunk ROMS octree split & Get/Set on template data recursive containers

--- src/Core/Volumetric/SparseOctree/Volumetric.SparseOctree.ROMSChunk.tImp.hpp
+++ src/Core/Volumetric/SparseOctree/Volumetric.SparseOctree.ROMSChunk.tImp.hpp
@@ -45,7 +45,58 @@ template< eLod2N LOD, typename Atomic >
 inline  void
 cROMSChunk<LOD,Atomic>::Set(tIndex iX,tIndex iY,tIndex iZ,const Atomic & iValue)
 {
-    // Todo: Depends on the type & subtype of data.
+    switch( mData->Type() )
+    {
+        case  eType::kOrdered:
+        {
+            if( mData->SubType() == eSubType::kEmpty )
+            {
+                if( LOD == mROMSConfig->MicroscopicLODGranularity() )
+                {
+                    delete  mData;
+                    mData = new  cEntropicRawData< LOD, Atomic >( mROMSConfig );
+                    mData->Set( iX, iY, iZ, iValue );
+                    break;
+                }
+                else if( LOD > mROMSConfig->MicroscopicLODGranularity() )
+                {
+                    delete  mData;
+                    mData = new  cSparseData< LOD, Atomic >( mROMSConfig );
+                    mData->Set( iX, iY, iZ, iValue );
+                    break;
+                }
+                else
+                {
+                    // Shouldn't be possible
+                    xassert( 0 );
+                }
+            }
+
+            if( mData->SubType() == eSubType::kFull )
+            {
+                break;
+            }
+        }
+
+        case  eType::kSparse:
+        {
+            break;
+        }
+
+        case  eType::kEntropic:
+        {
+            if( mData->SubType() == eSubType::kRaw )
+            {
+                break;
+            }
+
+            if( mData->SubType() == eSubType::kRLE )
+            {
+                break;
+            }
+        }
+
+    }
     mData->Set( iX, iY, iZ, iValue );
 }
 
